a39f1626fbbb96f0a2539ad86a84241090362748
Vadim Sachkov <vadimsachkov@gmail.com>
2023-07-12 08:08:55 +0300

    20230712-01

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import re
+import os
 
-def parse_backup_file(filename):
+def parse_backup_file(filepath):
     result = {}
 
-    with open(filename, 'r') as file:
+    with open(filepath, 'r') as file:
         command_name = ''
         command_body = ''
 
@@ -30,6 +31,34 @@ def write_dict_to_file(dictionary, filename):
         for value in dictionary.values():
             file.write(str(value) )
 
+
+def write_dict_to_files(data_dict, filename, size):
+    file_number = 1
+    # Получаем базовое имя файла и расширение
+    file_name, file_ext = os.path.splitext(filename)
+    # Создаем новое имя файла с добавленной переменной var
+    current_file = f"{file_name}__{file_number}{file_ext}"
+    with open(current_file, "w") as file:
+        file_size = 0
+
+        # Записываем значения словаря в файл
+        for key, value in data_dict.items():
+            line = f"{value}"
+
+            # Проверяем, не превышен ли размер файла
+            if file_size + len(line) <= size:
+                file.write(line)
+                file_size += len(line)
+            else:
+                # Если размер превышен, закрываем текущий файл
+                # и открываем следующий с новым номером
+                file.close()
+                file_number += 1
+                current_file = f"{file_name}__{file_number}{file_ext}"
+                with open(current_file, "w") as new_file:
+                    new_file.write(line)
+                    file_size = len(line)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     backup_filename = "C:\\Users\\user\\Desktop\\2.rsc"
@@ -37,5 +66,5 @@ if __name__ == '__main__':
     for name, command_body in parsed_commands.items():
         print(f'Command: {name}')
         #print(f'Body:\n{command_body}\n')
-    write_dict_to_file(parsed_commands,"C:\\Users\\user\\Desktop\\2_.rsc")
+    write_dict_to_files(parsed_commands,backup_filename,50000)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
