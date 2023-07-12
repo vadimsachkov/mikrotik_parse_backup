4163cd53adee672104ef9ffb1fd566c526e25f13
Vadim Sachkov <vadimsachkov@gmail.com>
2023-07-12 23:01:40 +0300

    20230712-02

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import os
+from tkinter import filedialog as fd
 
 def parse_backup_file(filepath):
     result = {}
@@ -38,33 +39,35 @@ def write_dict_to_files(data_dict, filename, size):
     file_name, file_ext = os.path.splitext(filename)
     # Создаем новое имя файла с добавленной переменной var
     current_file = f"{file_name}__{file_number}{file_ext}"
-    with open(current_file, "w") as file:
-        file_size = 0
+    file=open(current_file, "w")
+    file_size = 0
 
-        # Записываем значения словаря в файл
-        for key, value in data_dict.items():
-            line = f"{value}"
+    # Записываем значения словаря в файл
+    for key, value in sorted(data_dict.items()):
+        line = f"{value}"
 
-            # Проверяем, не превышен ли размер файла
-            if file_size + len(line) <= size:
-                file.write(line)
-                file_size += len(line)
-            else:
-                # Если размер превышен, закрываем текущий файл
-                # и открываем следующий с новым номером
-                file.close()
-                file_number += 1
-                current_file = f"{file_name}__{file_number}{file_ext}"
-                with open(current_file, "w") as new_file:
-                    new_file.write(line)
-                    file_size = len(line)
+        # Проверяем, не превышен ли размер файла
+        if size<=0 or file_size + len(line) <= size:
+            file.write(line)
+            file_size += len(line)
+        else:
+            # Если размер превышен, закрываем текущий файл
+            # и открываем следующий с новым номером
+            file.close()
+            file_number += 1
+            current_file = f"{file_name}__{file_number}{file_ext}"
+            file=open(current_file, "w")
+            file.write(line)
+            file_size = len(line)
+    file.close()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    backup_filename = "C:\\Users\\user\\Desktop\\2.rsc"
+    backup_filename=fd.askopenfilename(title="Выберите backup, экспортируемый из микротик")
+    #backup_filename = "C:\\Users\\user\\Desktop\\Новая папка (4)\\2.rsc"
     parsed_commands = parse_backup_file(backup_filename)
     for name, command_body in parsed_commands.items():
         print(f'Command: {name}')
         #print(f'Body:\n{command_body}\n')
-    write_dict_to_files(parsed_commands,backup_filename,50000)
+    write_dict_to_files(parsed_commands,backup_filename,0)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
